7362a556a2832d4dbf4445ac2f7acb7dea422833
Prince Nwaimo <nwaimoprince12@gmail.com>
2023-06-30 11:26:30 +0100

    first commit

---
+++ src/main/java/com/example/crowapp/CrowAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.crowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crowapp/CrowAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crowapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
